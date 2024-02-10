40f08df7fcad2986eed3d4bc9eeb4c02eee7966f
mahendra761 <mahendragawande90@gmail.com>
2024-02-10 21:06:26 +0530

    create Beauty section

--- app js/swiper.js
+++ app js/swiper.js
@@ -33,7 +33,7 @@ const initSlider = () => {
   const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
   const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
-  // Slide images according to the slide button clicks
+
   slideButtons.forEach(button => {
     button.addEventListener("click", () => {
       const direction = button.id === "prev-slide" ? -1 : 1;
@@ -42,13 +42,13 @@ const initSlider = () => {
     });
   });
 
-  // Show or hide slide buttons based on scroll position
+ 
   const handleSlideButtons = () => {
     slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
     slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
   }
 
-  // Call this function when image list scrolls
+
   imageList.addEventListener("scroll", () => {
     handleSlideButtons();
   });
---
+++ app js/toysec.js
@@ -0,0 +1,43 @@
+const initSlider = () => {
+    const sliders = document.querySelectorAll(".slider-wrapper");
+  
+    sliders.forEach(slider => {
+      const imageList = slider.querySelector(".image-list");
+      const slideButtons = slider.querySelectorAll(".slide-button");
+      const sliderScrollbar = slider.querySelector(".container .slider-scrollbar");
+      const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+  
+      slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+          const direction = button.id === "prev-slide" ? -1 : 1;
+          const scrollAmount = imageList.clientWidth * direction;
+          imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+      });
+  
+      const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+      }
+  
+      imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+      });
+  
+      handleSlideButtons(); 
+  
+      // event listener for window resize
+      window.addEventListener("resize", () => {
+        handleSlideButtons();
+      });
+  
+      //  event listener for window load
+      window.addEventListener("load", () => {
+        handleSlideButtons();
+      });
+    });
+  }
+  
+
+  initSlider();
+  
