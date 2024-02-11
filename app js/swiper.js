b8e973f967be7b03e26085b645ad6e91607689d8
mahendra761 <mahendragawande90@gmail.com>
2024-02-12 00:27:22 +0530

    update js files

--- app js/swiper.js
+++ app js/swiper.js
@@ -26,33 +26,3 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// top
-const initSlider = () => {
-  const imageList = document.querySelector(".slider-wrapper .image-list");
-  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-  const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
-  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
-
-
-  slideButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const direction = button.id === "prev-slide" ? -1 : 1;
-      const scrollAmount = imageList.clientWidth * direction;
-      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-    });
-  });
-
- 
-  const handleSlideButtons = () => {
-    slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
-    slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
-  }
-
-
-  imageList.addEventListener("scroll", () => {
-    handleSlideButtons();
-  });
-}
-
-window.addEventListener("resize", initSlider);
-window.addEventListener("load", initSlider);
--- app js/toysec.js
+++ app js/toysec.js
@@ -41,3 +41,4 @@ const initSlider = () => {
 
   initSlider();
   
+  
